bde5eb30814666d6d5e4fdd1464805bc2fbbf021
Dhananjay Kulkarni <dnkulkarni30@gmail.com>
2018-04-30 17:46:39 -0400

    global count

--- src/Components/Toy/Toy.js
+++ src/Components/Toy/Toy.js
@@ -5,17 +5,23 @@ class  Toy extends Component {
     
     constructor(props) {
         super(props);
-        this.state = { toySubscrCnt: 0 };
+        this.state = { toySubscrCnt: 0,globalCount:0 };
         this.Subscribed = this.Subscribed.bind(this);
         this.removeaToy = this.removeaToy.bind(this);
+       
       }
 
     Addtoy(){       
         console.log('comp added');       
     }
 
+   
     Subscribed(nm) {
-                    this.setState({ toySubscrCnt: this.state.toySubscrCnt+1 });                   
+                    this.setState({ toySubscrCnt: this.state.toySubscrCnt+1 });  
+                    this.props.inrGlobalCnt();                 
+                    var gl = this.props.getGlobalCnt();
+                    console.log('glb count ='  +gl);
+                    this.setState({globalCount:gl});
     }
   
     removeaToy()
@@ -26,10 +32,10 @@ class  Toy extends Component {
         return (
         <div className="maindiv"> 
                 <div>
-                    <div className="toyhdr"> a Toy [ id = {this.props.index}, key={this.props.key}]  </div>
+                    <div className="toyhdr"> a Toy [ id = {this.props.index}]  </div>
                     <div className="cntContainer">
                         <div className="cnt clr16877C"> L Count:{this.state.toySubscrCnt}</div>
-                        <div className="cnt clrB81651"> G Count:  </div>
+                        <div className="cnt clrB81651"> G Count:{this.state.globalCount}  </div>
                     </div>
                 </div>
                 <div className="subscribe">
--- src/Components/Toy/ToyContainer.js
+++ src/Components/Toy/ToyContainer.js
@@ -8,12 +8,29 @@ class  ToyContainer extends Component {
     constructor(props) 
     {
         super(props);
-        this.state = {toys:[] };
+        this.state = {toys:[], globalCnt:0 };
 
         this.addToy = this.addToy.bind(this);
         this.removeToy = this.removeToy.bind(this);
+        this.getGlobalCount = this.getGlobalCount.bind(this);
+        this.increaseGlobalCount = this.increaseGlobalCount.bind(this);
+
         this.addToy();
 
+    }
+
+    getGlobalCount()
+    {
+        console.log('global cnt get');
+        return this.state.globalCnt;
+            
+    }
+    increaseGlobalCount()
+    {
+        let gcnt = this.state.globalCnt;
+        gcnt++;
+        this.setState({globalCnt:gcnt});
+        console.log('global cnt increased : new value' + gcnt)
     }
 
     removeToy(ind)
@@ -28,7 +45,11 @@ class  ToyContainer extends Component {
     addToy(){
        
         let ltoy =  this.state.toys;
-        ltoy.push(<Toy key={this.state.toys.length} index={this.state.toys.length} simplemethod ={this.removeToy}  />);
+        ltoy.push(<Toy key={this.state.toys.length} index={this.state.toys.length} 
+                    simplemethod ={this.removeToy}  
+                    inrGlobalCnt = {this.increaseGlobalCount}
+                    getGlobalCnt = {this.getGlobalCount}
+                    />);
         this.setState({ toys:ltoy  });                
     }
 
